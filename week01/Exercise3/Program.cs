570318a41cda2987a19d51f997fa55caaf82e446
kalegraphics <kalejaiyeadeshina@gmail.com>
2025-05-08 07:31:19 +0100

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Main game loop
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes")
+        {
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int guess = -1;
+            int guessCount = 0;
+
+            // Guessing loop
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask if user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
